83c48bd0eb7ceca8a3358baf2343f68d18974c02
Khurram Shehzad <khurram.pak522@gmail.com>
2017-08-09 13:22:28 +0500

    Implement capture session with inputs and outputs

--- SceneTextRecognitioniOS/ViewController.swift
+++ SceneTextRecognitioniOS/ViewController.swift
@@ -6,18 +6,81 @@
 //  Copyright © 2017 devcrew. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
+import Vision
+
 
 class ViewController: UIViewController {
 
 override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    if isAuthorized() {
+        configureCamera()
+    }
 }
 
 override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
 }
+private func configureCamera() {
+    preview.session = session
+    let cameraDevices = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back)
+    var cameraDevice: AVCaptureDevice?
+    for device in cameraDevices.devices {
+        if device.position == .back {
+            cameraDevice = device
+            break
+        }
+    }
+    do {
+        let captureDeviceInput = try AVCaptureDeviceInput(device: cameraDevice!)
+        if session.canAddInput(captureDeviceInput) {
+            session.addInput(captureDeviceInput)
+        }
+    }
+    catch {
+        print("Error occured \(error)")
+        return
+    }
+    session.sessionPreset = .high
+    let videoDataOutput = AVCaptureVideoDataOutput()
+    videoDataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "Buffer Queue", qos: .userInteractive, attributes: .concurrent, autoreleaseFrequency: .inherit, target: nil))
+    if session.canAddOutput(videoDataOutput) {
+        session.addOutput(videoDataOutput)
+    }
+    preview.videoPreviewLayer.videoGravity = .resize
+    session.startRunning()
+}
+private var preview: PreviewView {
+    return view as! PreviewView
+}
+private func isAuthorized() -> Bool {
+    let authorizationStatus = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
+    switch authorizationStatus {
+    case .notDetermined:
+        AVCaptureDevice.requestAccess(for: AVMediaType.video,
+                                      completionHandler: { (granted:Bool) -> Void in
+                                        if granted {
+                                            DispatchQueue.main.async {
+                                                self.configureCamera()
+                                            }
+                                        }
+        })
+        return true
+    case .authorized:
+        return true
+    case .denied, .restricted: return false
+    }
+}
+private let session = AVCaptureSession()
 }
 
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+// MARK: - Camera Delegate and Setup
+func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+    
+}
+}
